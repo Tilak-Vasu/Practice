f377b325ccfd052a2cb3c1938f9e4baef4f84413
Tilak Vasu <tilak.vasu@thegatewaycorp.co.in>
2025-10-03 13:32:16 +0530

    6 commit

--- 6.js
+++ 6.js
@@ -1,39 +1,66 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 function Todo() {
-    const [inp,setinp] = useState ('');
-    const [tlist , setlist] = useState([]);
+  const [inp, setInp] = useState('');
+  const [tlist, setList] = useState([]);
 
-    const hIc = (e) => {
-        setinp(e.target.value);
-    };
-    const add = () => {
-        if(inp.trim() !== ''){
-            setlist([...tlist,inp]);
-            setinp('');
-        }
-    };
-    return(
-        <div>
-        <h1> Todo List</h1>
-            <input 
-                type="text"
-                value = {inp}
-                onChange={hIc}
-                />
-            <br />
-            <br />
-            <button onClick={add}>Add task</button>
-            <ul>
-                {tlist.map((todo,index)=>(
-                        <li key={index}> {todo}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  const hIc = (e) => {
+    setInp(e.target.value);
+  };
+
+  const add = () => {
+    if (inp.trim() !== '') {
+      setList([...tlist, { text: inp, completed: false }]);
+      setInp('');
+    }
+  };
+
+  const toggle = (index) => {
+    const newList = tlist.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setList(newList);
+  };
+
+  const deletetask = (index) => {
+    const newList = tlist.filter((_, i) => i !== index);
+    setList(newList);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={inp} onChange={hIc} />
+      <br />
+      <br />
+      <button onClick={add}>Add task</button>
+      <ul>
+        {tlist.map((todo, index) => (
+          <li
+            key={index}
+            style={{
+              textDecoration: todo.completed ? 'line-through' : 'none',
+              cursor: 'pointer',
+            }}
+          >
+            <span onClick={() => toggle(index)}>{todo.text}</span>
+            <button
+              onClick={() => deletetask(index)}
+              style={{ marginLeft: '10px' }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default Todo;
 
+export default Todo;
 
 
 // import Todo from './todolist'; 
